efd20853aaddd8d2f04f78575f63996abdfca9a4
Shakhawat Hossain <whoshakhawat@gmail.com>
2020-02-03 14:42:13 +0600

    Flutter demo Firebase Login App

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:firebaseWeb/auth_service.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthService().handleAuth(),
+    );
+  }
+}
